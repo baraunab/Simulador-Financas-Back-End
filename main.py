fdf122ffbe2b2f82a4dfef0ef82788c527a2a00c
kprincipe <kinanprincipes@gmail.com>
2025-09-27 18:05:37 -0400

    Implementacao dos Endpoints

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
-from typing import Union
-
+# importacao de bibliotecas
 from fastapi import FastAPI
+from pydantic import BaseModel
 
+# modelagem da estrutura de dados cdb recebida pelo frontend
+class Cdb(BaseModel):
+    investimento: float
+    taxa: float
+    vencimento: int
+    res: int
+
+# modelagem da estrutura de dados tesouro recebida pelo frontend
+class Tesouro(BaseModel):
+    investimento: float
+    taxa: float
+    aporte_mensal: float
+    vencimento: int
+    
+# TODO: implementar calculo do tesouro
+def calcular_tesouro(tesouro: Tesouro):
+    return {"mensagem": "nao implementado"}
+
+# TODO: implementar calculo do cdb
+def calcular_cdb(tesouro: Tesouro):
+    return {"mensagem": "nao implementado"}
+
+# instanciacao da biblioteca FastAPI
 app = FastAPI()
 
-
+# routing pro endereco raiz
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"mensagem": "em manutencao"}
 
+# routing para operacao do simulador
+@app.post("/cdb")
+def calcular(cdb: Cdb):
+    return calcular_cdb(cdb)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# routing para operacao do simulador
+@app.post("/tesouro")
+def calcular(tesouro: Tesouro):
+    return calcular_tesouro(tesouro)
