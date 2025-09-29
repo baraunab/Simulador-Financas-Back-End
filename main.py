58987cb94750cc8b4bf7b59f4ff400ca43261b7d
kprincipe <kinanprincipes@gmail.com>
2025-09-28 23:32:29 -0400

    Modelo de dados atualizado

--- main.py
+++ main.py
@@ -5,24 +5,26 @@ from pydantic import BaseModel
 # modelagem da estrutura de dados cdb recebida pelo frontend
 class Cdb(BaseModel):
     investimento: float
+    lucro_liquido: float
     taxa: float
-    vencimento: int
-    res: int
+    vencimento: str
 
 # modelagem da estrutura de dados tesouro recebida pelo frontend
 class Tesouro(BaseModel):
     investimento: float
-    taxa: float
     aporte_mensal: float
-    vencimento: int
+    lucro_bruto: float
+    lucro_liquido: float
+    titulo: str
+    vencimento: str
     
 # TODO: implementar calculo do tesouro
 def calcular_tesouro(tesouro: Tesouro):
-    return {"mensagem": "nao implementado"}
+    return {"mensagem": "tesouro nao implementado"}
 
 # TODO: implementar calculo do cdb
-def calcular_cdb(tesouro: Tesouro):
-    return {"mensagem": "nao implementado"}
+def calcular_cdb(cdb: Cdb):
+    return {"mensagem": "cdb nao implementado"}
 
 # instanciacao da biblioteca FastAPI
 app = FastAPI()
